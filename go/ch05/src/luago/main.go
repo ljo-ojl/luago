3ea5e5130b50f0c41e7bd4c298dcd4c4bc5a0cc1
ljo-ojl <906596025@qq.com>
2022-07-26 23:21:45 +0800

    ch05

--- go/ch05/src/luago/main.go
+++ go/ch05/src/luago/main.go
@@ -1,31 +1,11 @@
 package main
 
 import "fmt"
-import . "luago/api"
-import _ "luago/binchunk"
-import "luago/state"
+
 
 func main() {
-	ls := state.New()
+	fmt.Println(IFloorDiv(-5, 3))
 
-	ls.PushBoolean(true)
-	printStack(ls)
-	ls.PushInteger(10)
-	printStack(ls)
-	ls.PushNil()
-	printStack(ls)
-	ls.PushString("hello")
-	printStack(ls)
-	ls.PushValue(-4)
-	printStack(ls)
-	ls.Replace(3)
-	printStack(ls)
-	ls.SetTop(6)
-	printStack(ls)
-	ls.Remove(-3)
-	printStack(ls)
-	ls.SetTop(-5)
-	printStack(ls)
 }
 
 func printStack(ls LuaState) {
