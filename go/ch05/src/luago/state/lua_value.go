97621ed2cbd3a4d94fa27766dbaeea2f3c26ac69
ljo-ojl <906596025@qq.com>
2022-07-27 00:50:13 +0800

    ch05

--- go/ch05/src/luago/state/lua_value.go
+++ go/ch05/src/luago/state/lua_value.go
@@ -14,3 +14,32 @@ func typeOf(val luaValue) LuaType {
 	default: 	panic("todo!")
 	}
 }
+
+func convertToFloat(val luaState) (float64, bool) {
+	switch x := val.(type) {
+	case float64: 	return x, true
+	case int64: 	return float64(x), true
+	case string:	return number.ParseFloat(x)
+	default:		return 0, false
+	}
+}
+
+func convertToInteger(val luaValue) (int64, bool) {
+	switch x := val.(type) {
+	case int64: 	return x, true
+	case float64:	return number.FloatToInteger(x)
+	case string:	return _stringToInteger(x)
+	default:		return 0, false
+	}
+}
+
+func _stringToInteger(s string)	(int64, bool) {
+	if i, ok := number.ParseInteger(s); ok {
+		return i, true
+	}
+	if f, ok := number.ParseFloat(s); ok {
+		return number.FloatToInteger(f)
+	}
+	return 0. false
+}
+
